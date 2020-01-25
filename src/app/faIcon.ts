72f3a8a0064ec005cb6c8c47014ed98f00014636
Wutthinan <wutthinan@odd.team>
2020-01-25 15:00:30 +0700

    Home page

---
+++ src/app/faIcon.ts
@@ -0,0 +1,12 @@
+import {faBarcode, faCogs, faCubes, faHandshake, faPuzzlePiece, faThumbsUp, faUsers, faWrench} from '@fortawesome/free-solid-svg-icons';
+
+export const faIcon = {
+  faCogs,
+  faBarcode,
+  faHandshake,
+  faUsers,
+  faPuzzlePiece,
+  faWrench,
+  faCubes,
+  faThumbsUp
+}
